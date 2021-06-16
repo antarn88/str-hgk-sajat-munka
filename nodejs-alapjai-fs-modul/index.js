26b611027fab9ed4e4e4189087ddcf330c115e9e
Arnold <antaloczi.arnold@gmail.com>
2021-06-16 19:12:58 +0200

    nodejs-alapjai-fs-modul: Second task completed

--- nodejs-alapjai-fs-modul/index.js
+++ nodejs-alapjai-fs-modul/index.js
@@ -1,4 +1,10 @@
 const { mkdir, writeFile } = require('fs').promises;
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'));
+const { createGzip } = require('zlib');
+const path = require('path');
+
+let newFileName = '';
 
 const createFileStructure = async () => {
   await mkdir('controllers');
@@ -10,4 +16,44 @@ const createFileStructure = async () => {
   await writeFile('./app.js', '');
 };
 
-createFileStructure();
+const ready = () => {
+  const packageName = `${newFileName}.gz`;
+
+  fs.unlink(newFileName, (error) => {
+    if (error) throw error;
+  });
+
+  fs.unlink(packageName, (error) => {
+    if (error) throw error;
+  });
+};
+
+const compressFile = (filename) => {
+  const compress = createGzip();
+  const input = fs.createReadStream(filename);
+  const output = fs.createWriteStream(`${filename}.gz`);
+
+  input.pipe(compress).pipe(output);
+  ready();
+};
+
+const filePacker = (filepath) => {
+  newFileName = `${path.basename(filepath, path.extname(filepath))}.bak`;
+
+  const readableStream = fs.createReadStream(filepath, {
+    encoding: 'utf-8',
+    highWaterMark: 10,
+  });
+
+  const writeableStream = fs.createWriteStream(`./${newFileName}`);
+
+  readableStream.pipe(writeableStream);
+  readableStream.on('end', () => compressFile(newFileName));
+};
+
+const init = () => {
+  createFileStructure();
+  filePacker('./text.txt');
+};
+
+init();
