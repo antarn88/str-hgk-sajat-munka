84e8c0da6c219ff3c126db5f559597bc7b1c5826
Arnold <antaloczi.arnold@gmail.com>
2021-07-11 17:30:12 +0200

    API-feladat-04: completed app

--- api-feladat-04/src/index.js
+++ api-feladat-04/src/index.js
@@ -1,18 +1,19 @@
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
 const express = require('express');
-
-const app = express();
-const port = 3000;
-
-// Instead of body-parser
-app.use(express.json());
-
-// Swagger
+const morgan = require('morgan');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 
+const logger = require('./config/logger');
+
+const app = express();
+const port = 3000;
 const swaggerDocument = YAML.load('./docs/swagger.yaml');
+
+app.use(morgan('tiny', { stream: logger.stream }));
+
+app.use(express.json());
 
 app.use('/person', require('./routes'));
 
@@ -25,14 +26,8 @@ app.use((err, req, res, next) => {
   } else {
     res.status(err.statusCode);
   }
-  console.error({
-    hasError: true,
-    message: err.message,
-  });
-  res.json({
-    hasError: true,
-    message: 'An error occurred.',
-  });
+  logger.error(err.message);
+  res.send('An error occurred.');
 });
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
