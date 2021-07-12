c5f24cc390759998c58478d0fb8b30172766322a
Arnold <antaloczi.arnold@gmail.com>
2021-07-12 16:59:55 +0200

    API-feladat-07: Initial commit

---
+++ api-feladat-07/src/config/logger.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-unused-vars */
+const { join } = require('path');
+const winston = require('winston');
+
+const options = {
+  file: {
+    level: process.env.LOG_LEVEL_FILE,
+    filename: join(__dirname, '../../app.log'),
+    format: winston.format.simple(),
+  },
+  console: {
+    level: process.env.LOG_LEVEL_CONSOLE,
+  },
+};
+
+const logger = winston.createLogger({
+  format: winston.format.simple(),
+  transports: [
+    new winston.transports.File(options.file),
+    new winston.transports.Console(options.console),
+  ],
+  exitOnError: false,
+});
+
+logger.stream = {
+  write: (message, encoding) => {
+    logger.info(message);
+  },
+};
+
+module.exports = logger;
