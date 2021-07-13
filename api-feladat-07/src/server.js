5398eea183052f0f7a622119aa875d11b2d15f1b
Arnold <antaloczi.arnold@gmail.com>
2021-07-13 14:23:52 +0200

    API-Feladat-07: Refactor code before integration tests

---
+++ api-feladat-07/src/server.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-unused-vars */
+const config = require('config');
+const express = require('express');
+const morgan = require('morgan');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const mongoose = require('mongoose');
+
+const logger = require('./config/logger');
+
+const app = express();
+
+const swaggerDocument = YAML.load('./docs/swagger.yaml');
+mongoose.Promise = global.Promise;
+
+// Connect to MongoDB database
+(async () => {
+  try {
+    const { host, user, password } = config.get('database');
+    const connectionString = `mongodb+srv://${user}:${password}@${host}`;
+    await mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+    logger.info('MongoDB connection has been established successfully.');
+  } catch (error) {
+    logger.error(error.message);
+    process.exit();
+  }
+})();
+
+app.use(morgan('tiny', { stream: logger.stream }));
+
+app.use(express.json());
+
+app.use('/person', require('./person.routes'));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  if (!err.statusCode) {
+    res.status(500);
+  } else {
+    res.status(err.statusCode);
+  }
+  logger.error(err.message);
+  res.send('An error occurred.');
+});
+
+module.exports = app;
